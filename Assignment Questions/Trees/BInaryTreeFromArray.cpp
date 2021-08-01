ce1bb35e36fe65283476f44f9c503f11d08efdf4
srjsnehanshu <57353992+srjsnehanshu@users.noreply.github.com>
2021-08-01 15:21:27 +0530

    tree

---
+++ Assignment Questions/Trees/BInaryTreeFromArray.cpp
@@ -0,0 +1,21 @@
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+}
+
+node* construct(int a[], int i, int n, Node* root)
+{
+    if(i < n)
+    {
+        Node *temp = new node(a[i]);
+        root = temp;
+
+        root -> left = construct(a, 2 * i + 1, n, root -> left);
+
+        root -> right = construct(a, 2 * i + 2, n, root -> right);
+    }
+
+    return root;
+}
